456904b64f83ed2e04c5c0e39153c6f1ca36118e
Ibrahim-S-9 <ibrahim.s@abceez.digital>
2025-03-10 15:41:10 +0100

    initial commit

---
+++ styles/globalVariables.js
@@ -0,0 +1,30 @@
+export const usethemeProvider = (isDarkMode) => {
+  const lightMode = {
+    primaryColor: "#8db0bb",
+    secondaryColor: "white",
+    accentColor: "#befaff",
+    textColor: "black",
+    textPlaceholderColor: "#dimgrey",
+    secondAccentColor: "#BEFAFF6B",
+    dangerColor: "red",
+    appBgColor: "#F3F7FF",
+    ratingStarColor: "yellow",
+    headerColor: "white",
+    addBtnColor: "#8db0bb",
+  };
+  const darkMode = {
+    primaryColor: "#333333",
+    secondaryColor: "#404040",
+    accentColor: "#4A4A4A",
+    textColor: "white",
+    textPlaceholderColor: "#DFDFDFCC",
+    secondAccentColor: "#4A4A4AB0",
+    dangerColor: "red",
+    appBgColor: "#222222",
+    ratingStarColor: "yellow",
+    headerColor: "#4A4A4AB0",
+    addBtnColor: "white",
+  };
+
+  return isDarkMode === true ? darkMode : lightMode;
+};
